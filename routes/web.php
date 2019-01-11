3cf7f363e3a6682498f2e6eaa9f755136d129b95
Firman Kurniawan <mrroz3r@gmail.com>
2019-01-11 15:21:37 +0700

    Model

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/contact', function () {
+    return view('contact');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
